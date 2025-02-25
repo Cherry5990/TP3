5060431390f856542e5db1168fb5a581c64677d8
Cherry5990 <maelyss.fronton@etu.univ-orleans>
2025-02-25 12:17:19 +0100

    Biblio Box : attribut Box.ceontents et methode Box.add, testBoxAdd

--- Box.java
+++ Box.java
@@ -1,4 +1,12 @@
+import java.util.ArrayList;
+
 public class Box {
 
+    ArrayList<String> contents = new ArrayList<String>();
+
     public Box(){System.out.println("Box créée");}
+
+    public void add(String aAjouter){
+        this.contents.add(aAjouter);
+    }
 }
